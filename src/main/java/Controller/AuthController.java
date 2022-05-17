756c6a8b136f7ad42b3fb4e22b102c1d547a6881
Cptn-Mitchell <mrnasirmitchell@gmail.com>
2022-05-17 14:00:02 -0400

    Auth Con rd

--- src/main/java/Controller/AuthController.java
+++ src/main/java/Controller/AuthController.java
@@ -1,4 +1,46 @@
 package Controller;
 
+import io.javalin.http.HttpCode;
+
 public class AuthController {
+public void handleRegister(Context ctx){
+    try{
+        String input = ctx.body();
+        ObjectMapper mapper = new ObjectMapper();
+        User user = mapper.realValue(input, User.class);
+        int id = authService.register(user);
+        if(id == 0){
+            ctx.status(HttpCode.INTERNAL_SERVER_ERROR);
+            ctx.result("Registration unsuccessful.");
+        }else{
+            stx.status(HttpCode.CREATED);
+            ctx.result("Registration successful.");
+        }
+    }catch (Exception e) {
+        ctx.status(HttpCode.INTERNAL_SERVER_ERROR);
+        if(!e.getMessage().isEmpty());
+    }
+    e.printStackTrace();
+    }
+public void handleLogin(Context ctx) {
+    String username = ctx.formParam("username");
+    String password = ctx.formParam("password");
+
+    if(Objects.equals(username,"") || Objects.equals(password, "")){
+        ctx.status(HttpCode.BAD_REQUEST);
+        ctx.result("Invalid Credentials");
+    }else{
+        User user = authService.login(username, password);
+        if(user != null){
+            ctx.status(HttpCode.ACCEPTED);
+            ctx.header("Access-Control-Expose-Headers", "Current-User");
+            ctx.header("Current-User", ""+user.getId());
+            ctx.result(user.getRole().toString());
+
+        }else{
+            ctx.status(HttpCode.BAD_REQUEST);
+            ctx.result("Invalid Credentials");
+        }
+    }
+}
 }
--- src/main/java/ReimbursementDriver.java
+++ src/main/java/ReimbursementDriver.java
@@ -1,4 +1,8 @@
+import Controller.AuthController;
+import Controller.ReimbursementController;
 import Controller.UserController;
+import io.javalin.Javalin;
+import io.javalin.core.JavalinConfig;
 
 
 public class ReimbursementDriver {
@@ -7,5 +11,36 @@ public class ReimbursementDriver {
         controller.startController();
 
 
+    }
+    public void start(int port){
+        this.app.start(port);
+    }
+    AuthController authController = new AuthController();
+    UserController userController = new UserController();
+    ReimbursementController reimbursementController = new ReimbursementController();
+    Javalin app = Javalin.create(JavalinConfig::enableCorsForAllOrigins).routes(()->{
+        path("login",()->{
+            post(authController::handleLogin);
+        });
+        path("register", ()-> {
+            post(authController::handleRegister);
+
+        });
+        path("users", () -> {
+        get(authController::handleGetUsers);
+
+        path("{id}", ()-> {
+            get(userController::handleGetUserByID);
+        });
+    });
+    path("reimbursements", () -> {
+        get(reimbursementController)::handleGetReimbursements);
+        put(reimbursementController::handleSubmit);
+        path ("{id}", ()->{
+            get(reimbursementController::handleGetReimbursementById);
+            put(rembursementController::handleProcess);
+         });
+        });
+    });
     }
 }
